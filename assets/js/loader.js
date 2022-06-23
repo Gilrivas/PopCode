c02e8e70f35544fa4f7091a477a245d7e2b7ae32
Gilrivas <jorged1509@gmail.com>
2022-06-23 15:22:46 +0200

    first commit

---
+++ assets/js/loader.js
@@ -0,0 +1,12 @@
+ var pan = document.getElementById('loadingScreen')
+var circle = document.getElementById('loadedScreen')
+
+setTimeout(() =>{
+    pan.classList.add('none')
+  
+}, 3000)
+
+setTimeout(() =>{
+    circle.classList.remove('none')
+}, 3000) 
+ 
